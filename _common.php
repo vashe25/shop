63129d71226a60896990be065dfedc0b9c8108ee
vashe <va.she25@gmail.com>
2015-03-08 23:07:17 +0300

    twig connected to _common.php

--- _common.php
+++ _common.php
@@ -1,4 +1,14 @@
 <?php
+require_once '/vendor/autoload.php';
+
+$loader = new Twig_Loader_Array(array(
+    'index' => 'Hello {{ name }}!',
+));
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('index', array('name' => 'Fabien'));
+exit;
+
 session_start();
 
 $goods = isset($_SESSION['goods']) ? $_SESSION['goods'] : array();

329380a86ad4b56206b9e199bef1c20b9f097683
vashe <va.she25@gmail.com>
2015-03-12 01:19:42 +0300

    catalog.php is controller, catalog.twig is view

--- _common.php
+++ _common.php
@@ -1,13 +1,11 @@
 <?php
 require_once '/vendor/autoload.php';
+Twig_Autoloader::register();
 
-$loader = new Twig_Loader_Array(array(
-    'index' => 'Hello {{ name }}!',
-));
+$loader = new Twig_Loader_Filesystem('views');
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('index', array('name' => 'Fabien'));
-exit;
+
 
 session_start();
 
--- catalog.php
+++ catalog.php
@@ -1,40 +1,13 @@
 <?php
 require_once '_common.php';
-require_once 'header.php';
-
 
 $products = $shop->selectProducts();
 if ($action = "sort_price") {
 	$products = $shop->selectProductsByPrice($order);
 }
-?>
-<div id="content">
-<h1>Products</h1>
-<hr>
-<table>
-	<tr>
-		<td rowspan="2">product id</td>
-		<td rowspan="2">category id</td>
-		<td rowspan="2">title</td>
-		<td rowspan="2">description</td>
-		<td>sort by price</td>
-		<td rowspan="2">image</td>
-		<td rowspan="2"><a target="_blank" href="cart.php">Go to cart</a></td>
-	</tr>
-	<tr><td><a href="catalog.php?action=sort_price&order=ASC">ASC</a>/<a href="catalog.php?action=sort_price&order=DESC">DESC</a></td></tr>
-	<? foreach ($products as $item) : ?>			
-		<tr>
-			<td><?=$item['id']?></td>
-			<td><?=$item['category_id']?></td>
-			<td><a href="detail.php?id=<?=$item['id']?>"><?=htmlspecialchars($item['title'])?></a></td>
-			<td><?=htmlspecialchars($item['description'])?></td>
-			<td><?=$item['price']?></td>
-			<td><img class="product" src="./<?=$item['image']?>"></td>
-			<td><a class="cart" href="catalog.php?action=add&id=<?=$item['id']?>" title="add to cart"></a></td>
-		</tr>
-	<? endforeach ?>
-</table>
-</div>
-<?
-require_once 'footer.php';
+
+//$template = $twig->loadTemplate('catalog.twig');
+//echo $template->render(array('products' => $products));
+echo $twig->render('catalog.twig', array('products' => $products));
+
 ?>

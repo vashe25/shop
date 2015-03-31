ece413593b6523a5987be73a687794cc3a4d5f24
vashe25 <va.she25@gmail.com>
2015-03-31 17:59:05 +0300

    PDO part 2: cart.php

--- cart.php
+++ cart.php
@@ -1,7 +1,6 @@
 <?php
 require_once '_common.php';
 
-
 $id = isset($_GET['id']) ? $_GET['id'] : 0;
 $action = isset($_GET['action']) ? $_GET['action'] : NULL;
 $order = isset($_GET['order']) ? $_GET['order'] : NULL;
@@ -37,45 +36,41 @@ $cost = 0;
 //проверим есть ли у нас массив с товарами, которые добавил пользователь в корзину
 if (isset($_SESSION['goods'])) {
 	
-	//достаем из сесии массив: 'ID продукта' => 'количество штук'
-	
 	//соберем супер-массив для проброски в шаблон вывода
+	//выбираем ключи из массива: 'ID продукта' => 'количество штук'
 	$goodsIds = array_keys($_SESSION['goods']);
 	
+	//разделяем запятыми: 'ID продукта','ID продукта','ID продукта'...
 	$goodsIdsComma = implode(',', $goodsIds);
+	
+	//выбираем из базы товары по 'ID продукта'
 	$sql = "SELECT * FROM products WHERE id IN($goodsIdsComma)";
 	$res = $db->query($sql);
-	$rows = $res->fetchAll(PDO::FETCH_ASSOC);
-
-	foreach ($rows as $key => $row) {
-		$count = $_SESSION['goods'][$row['id']];
-		$rows[$key]['count'] = $count;
+	$product = $res->fetchAll(PDO::FETCH_ASSOC);
+	
+	//добавляем в массив $продукта строку о количестве штук, заказанных пользователем
+	foreach ($product as $key => $row) {
+		$item = $_SESSION['goods'][$row['id']];
+		$product[$key]['item'] = $item;
 	}
 
-	print '<pre>';
-	var_dump($_SESSION['goods']);
-	var_dump($rows);
-
-	//добавляем в массив $продукта строку о количестве штук, заказанных пользователем
-	//$array['item'] = $item;
-	//собираем все массивы $продуктов в один супер-массив, где ключем является ID продукта
-	//$product["$key"]= $array;
-	
 	//посчитаем пользователю его заказ
-	//foreach ($product as $value) {
-		//считаем количество товаров
-		//$items = $value['item'] + $items;
-		//считаем общую стоимость заказа
-		//$cost = $value['item'] * $value['price'] + $cost;
+	foreach ($product as $value) {
 		
-	//}
+		//считаем количество товаров
+		$items = $value['item'] + $items;
+		
+		//считаем общую стоимость заказа
+		$cost = $value['item'] * $value['price'] + $cost;
+		
+	}
 	
-	var_dump($array);
 	//пробрасываем в шаблон вывода супер-массив и посчитанные: количество и стоимость заказа
-	//echo $twig->render('cart.twig', array('product' => $product, 'items' => $items, 'cost' => $cost));
+	echo $twig->render('cart.twig', array('product' => $product, 'items' => $items, 'cost' => $cost));
 }
 
 else {
+	
 	//запускаем шаблон, в случае пустой корзины
 	echo $twig->render('cart.twig');
 }

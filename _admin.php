be0fb554b2ac01909d0717e31b18259eb1841e1c
vashe25 <va.she25@gmail.com>
2015-06-05 19:08:50 +0300

    login.php progress, + login.twig added

--- _admin.php
+++ _admin.php
@@ -1,11 +1,19 @@
 <?php
+session_start();
 
-//стартуем!
+//проверяем залогинился ли пользователь
+if (!isset($_SESSION['login'])) {
+	header('Location: login.php');
+}
+//
+
+//стартуем twig!
 require_once 'vendor/autoload.php';
 Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem('views');
 $twig = new Twig_Environment($loader);
+//
 
 //подключаемся к базе данных
 $opt = array(
@@ -14,4 +22,4 @@ $opt = array(
 );
 
 $db = new PDO('mysql:host=localhost;dbname=my','root', '', $opt);
-
+//
--- login.php
+++ login.php
@@ -1,4 +1,20 @@
 <?php
+session_start();
+
+//стартуем twig!
+require_once 'vendor/autoload.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('views');
+$twig = new Twig_Environment($loader);
+//
+
+/*/проверяем залогинился ли пользователь
+if (!isset($_SESSION['login'])) {
+	header('Location: login.php');
+}
+/*/
+
 /*/создание таблицы "users"
 $query = "CREATE TABLE `my`.`users`(  
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -17,11 +33,8 @@ $opt = array(
 $db = new PDO('mysql:host=localhost;dbname=my','root', '', $opt);
 //
 
-$md5_pass = md5($_GET['pass']);
-$login = $_GET['login'];
-echo "<pre>";
-var_dump($login);
-var_dump($md5_pass);
+$md5_pass = md5($_POST['pass']);
+$login = $_POST['login'];
 
 //получаем содержимое таблицы 'users' в массив
 $query = "SELECT * FROM users ORDER BY id ASC";
@@ -29,27 +42,78 @@ $stmnt = $db->prepare($query);
 $stmnt->execute();
 $stmnt = $stmnt->fetchAll();
 //
-echo "<pre>";
-var_dump($stmnt);
 
-//проверяем существует ли 'login' в таблице 'users'
-foreach ($stmnt as $value) {
-	if ($login == $value['login']) {
-		echo "user is set";
+switch ($_POST['action']) {
+		
+		//логинимся
+	case 'login':
+
+		//проверяем существует ли 'login' в таблице 'users'
+		foreach ($stmnt as $value) {
+			//проверяем логин введенный пользователем
+			if ($login == $value['login']) {
+				//проверяем пароль введенный пользователем
+				if ($md5_pass == $value['password']) {
+					//записываем в сессию логин и пароль пользователя
+					$_SESSION['login'] = $login;
+					$_SESSION['password'] = $md5_pass;
+					//
+
+					//введенные данные верны
+					$status['login'] = TRUE;
+					echo "Logged In!";
+
+					header('Location: admin.php');
+					break;
+				}
+			}
+			else {
+				//пользователь ввел не верные данные
+				$status['login'] = FALSE;
+				echo "Error: entered login or password isn't correct";
+				break;
+			}
+		}
+		//
 		break;
-	}
-	else {
-		echo "continue registration";
 
-		/*/добавление пользователя в базу данных
-		$query = "INSERT INTO users SET login = :login, password = :password";
-		$stmnt = $db->prepare($query);
-		$stmnt->execute(array(':login' => $login, ':password' => $md5_pass));
-		/*/
+		//выходим из сессии
+	case 'exit':
+		
+		//стираем логин и пароль пользователя из сессии
+		unset($_SESSION['login']);
+		unset($_SESSION['password']);
+		//
+		header('Location: login.php');
 
 		break;
-	}
+
+		//регистрируемся
+	case 'register':
+		
+		//проверяем существует ли 'login' в таблице 'users'
+		foreach ($stmnt as $value) {
+			if ($login == $value['login']) {
+				echo "Error: user already registred";
+				break;
+			}
+			else {
+
+				//добавление пользователя в базу данных
+				$query = "INSERT INTO users SET login = :login, password = :password";
+				$stmnt = $db->prepare($query);
+				$stmnt->execute(array(':login' => $login, ':password' => $md5_pass));
+				//
+				
+				echo "Success: user registered";
+				break;
+			}
+		}
+		//
+
+		break;
 }
-//
+
+echo $twig->render('login.twig', array('products' => $products));
 
 ?>

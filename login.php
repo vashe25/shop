c5cd33beaf746a80f13d5f93167826d7c476e15c
vashe25 <va.she25@gmail.com>
2015-06-19 20:24:17 +0300

    login.php Activation addon

--- login.php
+++ login.php
@@ -16,12 +16,14 @@ if ($_SESSION['login'] == NULL) {
 /*/
 
 /*/создание таблицы "users"
-$query = "CREATE TABLE `my`.`users`(  
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `login` VARCHAR(16) NOT NULL,
-  `password` VARCHAR(32) NOT NULL,
+$query = "CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `activation` tinyint(1) NOT NULL,
+  `hash` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=INNODB CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
 /*/
 
 //подключаемся к базе данных
@@ -100,45 +102,103 @@ switch ($action) {
 		$pass = $_POST['pass'];
 		$md5_pass = md5($pass);
 		$login = $_POST['login'];
+		$hash = $login . $md5_pass;
+		$hash = md5($hash);
 		//
 
-		//получаем содержимое таблицы 'users' в массив
+		//получаем содержимое таблицы users в массив
 		$query = "SELECT * FROM users ORDER BY id ASC";
 		$stmnt = $db->prepare($query);
 		$stmnt->execute();
 		$stmnt = $stmnt->fetchAll();
 		//
 		
-		//проверяем существует ли 'login' в таблице 'users'
-		foreach ($stmnt as $value) {
-			if ($login == $value['login']) {
-				
-				//этот логин уже занят
-				$error = "Error: user already registred. Choose another name.";
-				echo $twig->render('login.twig', array('login' => $login, 'password' => $pass, 'error' => $error));
+		//в случае пустой таблицы
+		if (empty($stmnt)) {
+			//добавление пользователя в базу данных
+			$query = "INSERT INTO users SET login = :login, password = :password, activation = '0', hash = :hash";
+			$stmnt = $db->prepare($query);
+			$stmnt->execute(array(':login' => $login, ':password' => $md5_pass, ':hash' => $hash));
+			//
+			
+			//отправим письмишко на почту пользователю с ссылкой активации
+			$message = '<html><head></head><body><p><a href=\"http://shop/login.php?action=activate&hash=' . $hash . '\">Push to activate your account!</a></p></body></html>';
+			$headers  = 'MIME-Version: 1.0' . "\r\n";
+			$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
-				break;
-			}
-			else {
-
-				//добавление пользователя в базу данных
-				$query = "INSERT INTO users SET login = :login, password = :password";
-				$stmnt = $db->prepare($query);
-				$stmnt->execute(array(':login' => $login, ':password' => $md5_pass));
-				//
-				
-				echo "Success: user registered";
-				break;
-			}
+			$mail = mail('somebody@ya.ru', 'Account activation', $message, $headers);
+			//
+			
+			$error = "Success: user registered";
+			echo $twig->render('login.twig', array('login' => $login, 'password' => 'Enter password', 'error' => $error));
 		}
 		//
+		else {
+			//проверяем существует ли login в таблице users
+			foreach ($stmnt as $value) {
+				if ($login == $value['login']) {
+					
+					//этот логин уже занят
+					$error = "Error: user already registred. Choose another name.";
+					echo $twig->render('login.twig', array('login' => $login, 'password' => $pass, 'error' => $error));
 
-		break;	
+					break;
+				}
+				else {
+
+					//добавление пользователя в базу данных
+					$query = "INSERT INTO users SET login = :login, password = :password, activation = '0', hash = :hash";
+					$stmnt = $db->prepare($query);
+					$stmnt->execute(array(':login' => $login, ':password' => $md5_pass, ':hash' => $hash));
+					//
+					
+					//отправим письмишко на почту пользователю с ссылкой активации
+					$message = '<html><head></head><body><p><a href=\"http://shop/login.php?action=activate&hash=' . $hash . '\">Push to activate your account!</a></p></body></html>';
+					$headers  = 'MIME-Version: 1.0' . "\r\n";
+					$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+
+					$mail = mail('somebody@ya.ru', 'Account activation', $message, $headers);
+					//
+					
+					$error = "Success: user registered";
+					echo $twig->render('login.twig', array('login' => $login, 'password' => 'Enter password', 'error' => $error));
+					
+					break;
+				}
+
+			}
+			//
+		}
+		
+		break;
+
+		//активируемся
+	case 'activate':
+		//берем хэш из ссылки
+		$hash = $_GET['hash'];
+
+		//ставим единичку в поле activation
+		$query = "UPDATE users SET activation = '1' WHERE hash = :hash";
+		$stmnt = $db->prepare($query);
+		$stmnt->execute(array(':hash' => $hash));	
+		//
+		
+		//достанем логин, который активируется
+		$row = $db->query("SELECT * FROM users WHERE id = " . $db->lastInsertId());
+		$row = $row->fetchAll();
+		//
+
+		$error = "Success: your account is activated";
+		echo $twig->render('login.twig', array('login' => $row['login'], 'error' => $error));
+		break;
+
 	default:
 		
 		echo $twig->render('login.twig', array());
 		
 		break;
 }
+
+unset($db);
 
 ?>
